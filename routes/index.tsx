a350ac9dcba645adeb456cc013df96d311aab674
maekawamikako <mikankounya@gmail.com>
2022-06-23 22:19:24 +0900

    index.tsxの編集

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,33 +4,20 @@ export default function Index() {
   return (
     <div className="screen index">
       <Head>
-        <title>Aleph.js</title>
+        <title>deno-shiritori</title>
         <meta name="description" content="The Fullstack Framework in Deno." />
       </Head>
-      <p className="logo">
-        <img src="/assets/logo.svg" width="75" height="75" title="Aleph.js" />
-      </p>
       <h1>
-        The Fullstack Framework in Deno.
+        ことばであそぼう
       </h1>
       <p>
-        <strong>Aleph.js</strong> gives you the best developer experience for building web applications<br />{" "}
-        with modern toolings.
+        でぃーのしりとりへようこそ<br />{" "}
+        ここではひらがなしかつかえません<br />{" "}
+        いっしょにしりとりであそびましょう
       </p>
-      <div className="external-links">
-        <a href="https://alephjs.org/docs/get-started" target="_blank">
-          Get Started
-        </a>
-        <a href="https://alephjs.org/docs" target="_blank">
-          Docs
-        </a>
-        <a href="https://github.com/alephjs/aleph.js" target="_blank">
-          Github
-        </a>
-      </div>
       <nav>
         <Link role="button" to="/todos">
-          Todos App Demo
+          げーむすたーと
         </Link>
       </nav>
     </div>
