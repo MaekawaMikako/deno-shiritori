aad601bf91687638606314bd89d0f16559318949
maekawamikako <mikankounya@gmail.com>
2022-06-25 19:00:29 +0900

    ディレクトリ名変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header.tsx";
-import "../style/app.css";
+import "../styles/app.css";
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
