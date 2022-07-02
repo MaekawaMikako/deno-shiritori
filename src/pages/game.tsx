a768571b0690529a4fd4145a071fdf1b56c00078
maekawamikako <mikankounya@gmail.com>
2022-07-03 01:36:45 +0900

    UI整え

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -47,7 +47,7 @@ export const data: Data<Store, Store> = {
 };
 
 export const Game = () => {
-  const step = 5;
+  const step = 3;
   const {
     data: { words },
     isMutating,
@@ -74,79 +74,92 @@ export const Game = () => {
         <title>Game</title>
         <meta name="description" content="しりとりする" />
       </Head>
-      {words.length > step &&
-        displayWordList.map((word, i) => (
-          <p className="recent-words">
-            {words.length - step + i} {word}
-          </p>
-        ))}
-      <p className="most-recent-word">
-        {words.length} {words.slice(-1)[0].message}
-      </p>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const form = e.currentTarget;
-          const fd = new FormData(form);
-          const message = fd.get("message")?.toString().trim();
-          const firstChar = (message ?? "").charAt(0);
-          const lastChar = words
-            .slice(-1)[0]
-            .message.charAt(words.slice(-1)[0].message.length - 1);
-          if (/[ん]$/.test(message ?? "")) {
-            return gameOver();
-          }
-          if (/[ぁぃぅぇぉっゃゅょゎ]/.test(message ?? "")) {
-            return alert("ぜんぶおおもじでかいてね");
-          }
-          if (/[^ぁ-ん]/.test(message ?? "")) {
-            return alert("ひらがなをつかってね\nきごうもだめだよ");
-          }
-          if (firstChar !== lastChar) {
-            return alert("まえのことばにつながってないよ");
-          }
-          if ((message ?? "").length < 2) {
-            alert("それはずるいからやめてほしいなぁ…");
-          }
-          if (message) {
-            await mutation.put(
-              { message },
-              {
-                // optimistic update data without waiting for the server response
-                optimisticUpdate: (data) => ({
-                  words: [...data.words, { id: 0, message, completed: false }],
-                }),
-                // replace the data with the new data that is from the server response
-                replace: true,
+      <div className="game-contents-container">
+        <div className="words-contents-container">
+          <div className="recent-words-container">
+            <div className="recent-words">
+              {words.length > step &&
+                displayWordList.map((word, i) => (
+                  <p className="recent-word">
+                    {words.length - step + i} {word}
+                  </p>
+                ))}
+            </div>
+            <p className="most-recent-word">
+              {words.length} {words.slice(-1)[0].message}
+            </p>
+          </div>
+        </div>
+        <div className="operation-conatainer">
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const form = e.currentTarget;
+              const fd = new FormData(form);
+              const message = fd.get("message")?.toString().trim();
+              const firstChar = (message ?? "").charAt(0);
+              const lastChar = words
+                .slice(-1)[0]
+                .message.charAt(words.slice(-1)[0].message.length - 1);
+              if (/[ん]$/.test(message ?? "")) {
+                return gameOver();
               }
-            );
-            const wordlist = words.map((obj) => obj.message);
-            setDisplayWordList(wordlist.slice(-step));
-            form.reset();
-            setTimeout(() => {
-              form.querySelector("input")?.focus();
-            }, 0);
-          }
-        }}
-      >
-        <input
-          type="text"
-          name="message"
-          placeholder="つぎのことばをにゅうりょくしてね"
-          autoFocus
-          autoComplete="off"
-          disabled={!!isMutating}
-        />
-      </form>
-      <div className="buttons">
-        <Button>
-          <Link role="button" to="/game">
-            はじめから
-          </Link>
-        </Button>
-        <Button onClick={() => setShowRule(true)}>
-          <a>るーる</a>
-        </Button>
+              if (/[ぁぃぅぇぉっゃゅょゎ]/.test(message ?? "")) {
+                return alert("ぜんぶおおもじでかいてね");
+              }
+              if (/[^ぁ-ん]/.test(message ?? "")) {
+                return alert("ひらがなをつかってね\nきごうもだめだよ");
+              }
+              if (firstChar !== lastChar) {
+                return alert("まえのことばにつながってないよ");
+              }
+              if ((message ?? "").length < 2) {
+                alert("それはずるいからやめてほしいなぁ…");
+              }
+              if (message) {
+                await mutation.put(
+                  { message },
+                  {
+                    // optimistic update data without waiting for the server response
+                    optimisticUpdate: (data) => ({
+                      words: [
+                        ...data.words,
+                        { id: 0, message, completed: false },
+                      ],
+                    }),
+                    // replace the data with the new data that is from the server response
+                    replace: true,
+                  }
+                );
+                const wordlist = words.map((obj) => obj.message);
+                setDisplayWordList(wordlist.slice(-step));
+                form.reset();
+                setTimeout(() => {
+                  form.querySelector("input")?.focus();
+                }, 0);
+              }
+            }}
+          >
+            <input
+              type="text"
+              name="message"
+              placeholder="つぎのことばをにゅうりょくしてね"
+              autoFocus
+              autoComplete="off"
+              disabled={!!isMutating}
+            />
+          </form>
+          <div className="buttons">
+            <Button>
+              <Link role="button" to="/game">
+                はじめから
+              </Link>
+            </Button>
+            <Button onClick={() => setShowRule(true)}>
+              <a>るーる</a>
+            </Button>
+          </div>
+        </div>
       </div>
       <Modal
         title="るーる"
